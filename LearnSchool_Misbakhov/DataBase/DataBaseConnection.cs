f3143a20ba2ebfe316e87df3f2e6f0affc23fab6
Student48 <tototoyo@yandex.ru>
2025-02-05 09:07:57 +0300

    Добавьте файлы проекта.

---
+++ LearnSchool_Misbakhov/DataBase/DataBaseConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnSchool_Misbakhov.DataBase
+{
+    public class DataBaseConnection
+    {
+        public static LearnShcoolMisbakhovEntities Connection { get; set; }
+    }
+}
